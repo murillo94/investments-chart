e6bbbb6a094af33e32f585cb15051ad35173a3ff
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-06-15 02:24:44 -0300

    Ajustando layout base para página de investimentos

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
-const InvestimentsPage = () => <div>todo</div>;
+const InvestimentsPage = () => (
+  <main>
+    <header>
+      <div>header 1</div>
+      <div>header 2</div>
+    </header>
+    <section>section</section>
+
+    <style jsx>
+      {`
+        main {
+          margin: 100px auto;
+          width: 600px;
+        }
+
+        header {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+
+        section {
+          margin-top: 50px;
+        }
+      `}
+    </style>
+  </main>
+);
 
 export default InvestimentsPage;
