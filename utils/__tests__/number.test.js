9be4f86e5876ff667041389064d329b109e7ff1a
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-06-15 13:35:34 -0300

    Criando funções utils para número

---
+++ utils/__tests__/number.test.js
@@ -0,0 +1,15 @@
+import { isNumber } from '../number';
+
+describe('Date', () => {
+  test('should return false when NaN', () => {
+    expect(isNumber('testing string')).toBe(false);
+    expect(isNumber(0)).toBe(false);
+    expect(isNumber(null)).toBe(false);
+    expect(isNumber(undefined)).toBe(false);
+  });
+
+  test('should return true when number', () => {
+    expect(isNumber(200)).toBe(true);
+    expect(isNumber(-200)).toBe(true);
+  });
+});
