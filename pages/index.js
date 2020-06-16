3432eb56bb37f063da49025469b04d88fa72ceba
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-06-15 23:46:49 -0300

    Exibindo dados no gráfico

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+
+import { Chart } from '../components/chart';
+
+import { formatData } from '../helpers/api';
+
 export async function getStaticProps() {
   const res = await fetch(
     'https://gist.githubusercontent.com/AgtLucas/a67c345e15c2eb3d4668c9b7e330ac44/raw/1de2450cbe69fde065bca9e498aaaaafcca61257/mock-data.js'
@@ -15,33 +21,39 @@ export async function getStaticProps() {
   };
 }
 
-const InvestimentsPage = () => (
-  <main>
-    <header>
-      <div>header 1</div>
-      <div>header 2</div>
-    </header>
-    <section>section</section>
+const InvestimentsPage = ({ data }) => {
+  const [investiments] = useState(formatData(data));
 
-    <style jsx>
-      {`
-        main {
-          margin: 100px auto;
-          width: 600px;
-        }
+  return (
+    <main>
+      <header>
+        <div>header 1</div>
+        <div>header 2</div>
+      </header>
+      <section>
+        <Chart data={investiments.data} />
+      </section>
 
-        header {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-        }
+      <style jsx>
+        {`
+          main {
+            margin: 100px auto;
+            width: 600px;
+          }
 
-        section {
-          margin-top: 50px;
-        }
-      `}
-    </style>
-  </main>
-);
+          header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          }
+
+          section {
+            margin-top: 50px;
+          }
+        `}
+      </style>
+    </main>
+  );
+};
 
 export default InvestimentsPage;
