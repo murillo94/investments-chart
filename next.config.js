49569379e40421971f2cd9b233cfe9608fb6172c
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-06-17 13:41:57 -0300

    Corrigindo build __tests__

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+const withPlugins = require('next-compose-plugins');
 const withOptimizedImages = require('next-optimized-images');
 
-module.exports = withOptimizedImages({
-  svgo: {
-    plugins: [{ removeComments: true, cleanupAttrs: true }]
+const plugins = [
+  [
+    withOptimizedImages,
+    {
+      svgo: {
+        plugins: [{ removeComments: true, cleanupAttrs: true }]
+      }
+    }
+  ]
+];
+
+module.exports = withPlugins(plugins, {
+  webpack: (config, { webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+    return config;
   }
 });
