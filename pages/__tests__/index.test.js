bacd5c95f9897af9e625cdb23396cba933b4c28f
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-06-17 13:21:39 -0300

    Melhorando teste

--- pages/__tests__/index.test.js
+++ pages/__tests__/index.test.js
@@ -2,6 +2,16 @@ import { render, fireEvent } from '@testing-library/react';
 
 import App from '../index';
 
+import {
+  PERIODS,
+  FROM_THE_BEGINNING,
+  LAST_MONTH,
+  THREE_MONTH,
+  SIX_MONTH,
+  ONE_YEAR,
+  TWO_YEAR
+} from '../../helpers/periods';
+
 const DATA = [
   {
     x: 1565308800000,
@@ -31,13 +41,44 @@ describe('App', () => {
     expect(getByText('Acumulado: R$ 114.956,80')).toBeInTheDocument();
     expect(getByText('Ganho: R$ 89.996,80')).toBeInTheDocument();
     expect(getByText('(78,29 %)')).toHaveStyle('color: #4ca64c');
+    expect(getByText('9 Ago 2019')).toBeInTheDocument();
+    expect(getByText('24 Set 2019')).toBeInTheDocument();
   });
 
   test('should have menu click and showing data graph', () => {
     const { getAllByText, getByText } = render(<App data={DATA} />);
 
-    fireEvent.click(getAllByText('desde o início')[0]);
-    fireEvent.click(getByText('2 anos'));
+    fireEvent.click(getAllByText(PERIODS[FROM_THE_BEGINNING])[0]);
+    fireEvent.click(getByText(PERIODS[LAST_MONTH]));
+
+    expect(getByText('Sem valor no período')).toBeInTheDocument();
+
+    fireEvent.click(getAllByText(PERIODS[LAST_MONTH])[0]);
+    fireEvent.click(getByText(PERIODS[THREE_MONTH]));
+
+    expect(getByText('Sem valor no período')).toBeInTheDocument();
+
+    fireEvent.click(getAllByText(PERIODS[THREE_MONTH])[0]);
+    fireEvent.click(getByText(PERIODS[SIX_MONTH]));
+
+    expect(getByText('Sem valor no período')).toBeInTheDocument();
+
+    fireEvent.click(getAllByText(PERIODS[SIX_MONTH])[0]);
+    fireEvent.click(getByText(PERIODS[ONE_YEAR]));
+
+    expect(getByText('Acumulado: R$ 114.956,80')).toBeInTheDocument();
+    expect(getByText('9 Ago 2019')).toBeInTheDocument();
+    expect(getByText('24 Set 2019')).toBeInTheDocument();
+
+    fireEvent.click(getAllByText(PERIODS[ONE_YEAR])[0]);
+    fireEvent.click(getByText(PERIODS[TWO_YEAR]));
+
+    expect(getByText('Acumulado: R$ 114.956,80')).toBeInTheDocument();
+    expect(getByText('9 Ago 2019')).toBeInTheDocument();
+    expect(getByText('24 Set 2019')).toBeInTheDocument();
+
+    fireEvent.click(getAllByText(PERIODS[TWO_YEAR])[0]);
+    fireEvent.click(getByText(PERIODS[FROM_THE_BEGINNING]));
 
     expect(getByText('Acumulado: R$ 114.956,80')).toBeInTheDocument();
     expect(getByText('9 Ago 2019')).toBeInTheDocument();
@@ -47,8 +88,8 @@ describe('App', () => {
   test('should have menu click and showing no data graph', () => {
     const { getAllByText, getByText } = render(<App data={DATA} />);
 
-    fireEvent.click(getAllByText('desde o início')[0]);
-    fireEvent.click(getByText('último mês'));
+    fireEvent.click(getAllByText(PERIODS[FROM_THE_BEGINNING])[0]);
+    fireEvent.click(getByText(PERIODS[LAST_MONTH]));
 
     expect(getByText('Sem valor no período')).toBeInTheDocument();
   });
@@ -61,7 +102,7 @@ describe('App', () => {
     fireEvent.keyDown(container, { key: 'ArrowDown', code: 'ArrowDown' });
     fireEvent.keyDown(container, { key: 'ArrowDown', code: 'ArrowDown' });
 
-    expect(getAllByText('último mês')[0]).toBeInTheDocument();
-    expect(getAllByText('último mês')[1]).toBeInTheDocument();
+    expect(getAllByText(PERIODS[LAST_MONTH])[0]).toBeInTheDocument();
+    expect(getAllByText(PERIODS[LAST_MONTH])[1]).toBeInTheDocument();
   });
 });
